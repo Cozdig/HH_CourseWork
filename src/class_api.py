496ece33eb04f9ce29134a34133b19dc909fa075
Cozdig <tank-20@list.ru>
2025-05-13 16:05:12 +0300

    second commit

--- src/class_api.py
+++ src/class_api.py
@@ -20,17 +20,17 @@ class HH(Parser):
     """
 
     def __init__(self, file_worker):
-        self.url = 'https://api.hh.ru/vacancies'
-        self.headers = {'User-Agent': 'HH-User-Agent'}
-        self.params = {'text': '', 'page': 0, 'per_page': 100}
-        self.vacancies = []
-        self.file_worker = file_worker
+        self.__url = 'https://api.hh.ru/vacancies'
+        self.__headers = {'User-Agent': 'HH-User-Agent'}
+        self.__params = {'text': '', 'page': 0, 'per_page': 100}
+        self.__vacancies = []
+        self.__file_worker = file_worker
         super().__init__()
 
     def load_vacancies(self, keyword):
-        self.params['text'] = keyword
-        while self.params.get('page') != 20:
-            response = requests.get(self.url, headers=self.headers, params=self.params)
+        self.__params['text'] = keyword
+        while self.__params.get('page') != 20:
+            response = requests.get(self.__url, headers=self.__headers, params=self.__params)
             vacancies = response.json()['items']
-            self.vacancies.extend(vacancies)
-            self.params['page'] += 1
+            self.__vacancies.extend(vacancies)
+            self.__params['page'] += 1
